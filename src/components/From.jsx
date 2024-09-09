1bcefc4808c8a9cda349abaa140111d80812e812
rezamovaheddi <rmwhdy8@gamil.com>
2024-09-09 23:07:14 +0330

    add item With array loop

--- src/components/From.jsx
+++ src/components/From.jsx
@@ -5,9 +5,9 @@ export default function From() {
     <form className='bg-[#e5771f] p-2 flex items-center justify-center gap-2'>
         <h3 className='mr-4 text-4xl'>What do you need for travel😍</h3>
         <select className='bg-[#ffebb3] [#5a3e2b] rounded-3xl font-bold text-3xl p-p-f'>
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
+          {Array.from({length: 20}, (_,i)=> i+1).map((num)=>(
+            <option value={num} key={num}>{num}</option>
+          ))}
         </select>
         <input className='bg-[#ffebb3] [#5a3e2b] rounded-3xl font-bold text-3xl p-p-f'  type='text' placeholder='Item...' />
         <button className='bg-[#76c7ad] [#5a3e2b] rounded-3xl font-bold text-3xl p-p-f uppercase'>Add</button>
